fd2ba465cfe203dc9dbeae992738b4656439ab65
jerrychen <jerry@gmail.com>
2022-07-09 11:27:09 +0800

    完成pm2.5繪製

--- static/js/pm25-chart.js
+++ static/js/pm25-chart.js
@@ -1,25 +1,168 @@
-var chartDom = document.querySelector('#main');
-var myChart = echarts.init(chartDom);
-var option;
+const chart1El = document.querySelector('#main');
+let chart1 = echarts.init(chart1El);
 
-option = {
-    xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [
+
+$(document).ready(() => {
+    drawPM25();
+});
+
+
+function drawPM25() {
+    chart1.showLoading();
+    $.ajax(
         {
-            data: [120, 200, 150, 80, 70, 110, 130],
-            type: 'bar',
-            showBackground: true,
-            backgroundStyle: {
-                color: 'rgba(180, 180, 180, 0.2)'
+            url: "/pm25-json",
+            type: "POST",
+            dataType: "json",
+            success: (data) => {
+                chart1.hideLoading();
+                console.log(data);
+                drawChart1(data);
+            },
+            error: () => {
+                chart1.hideLoading();
+                alert("資料讀取失敗!");
             }
         }
-    ]
-};
+    );
+}
 
-option && myChart.setOption(option);
+function drawChart1(datas) {
+    var option;
+    option = {
+        title: {
+            text: 'PM25全台資訊'
+        },
+        toolbox: {
+            show: true,
+            orient: 'vertical',
+            left: 'left',
+            top: 'center',
+            feature: {
+                magicType: { show: true, type: ['line', 'bar', 'tiled'] },
+                restore: { show: true },
+                saveAsImage: { show: true }
+            }
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['PM2.5']
+        },
+        xAxis: {
+            type: 'category',
+            data: datas['stationName']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: 'PM2.5',
+                data: datas['result'],
+                type: 'bar',
+                showBackground: true,
+                backgroundStyle: {
+                    color: 'rgba(180, 180, 180, 0.2)'
+                }
+            }
+        ]
+    };
+
+    option && chart1.setOption(option);
+}
+
+function darwChart2(datas) {
+
+    var option;
+
+    // prettier-ignore
+    let dataAxis = datas["stationName"];
+    // prettier-ignore
+    let data = datas["result"]
+    let yMax = 500;
+    let dataShadow = [];
+    for (let i = 0; i < data.length; i++) {
+        dataShadow.push(yMax);
+    }
+    option = {
+        title: {
+            text: '特性示例：渐变色 阴影 点击缩放',
+            subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
+        },
+        xAxis: {
+            data: dataAxis,
+            axisLabel: {
+                inside: true,
+                color: '#fff'
+            },
+            axisTick: {
+                show: false
+            },
+            axisLine: {
+                show: false
+            },
+            z: 10
+        },
+        yAxis: {
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+            axisLabel: {
+                color: '#999'
+            }
+        },
+        dataZoom: [
+            {
+                type: 'inside'
+            }
+        ],
+        series: [
+            {
+                type: 'bar',
+                showBackground: true,
+                itemStyle: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                        { offset: 0, color: '#83bff6' },
+                        { offset: 0.5, color: '#188df0' },
+                        { offset: 1, color: '#188df0' }
+                    ])
+                },
+                emphasis: {
+                    itemStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                            { offset: 0, color: '#2378f7' },
+                            { offset: 0.7, color: '#2378f7' },
+                            { offset: 1, color: '#83bff6' }
+                        ])
+                    }
+                },
+                data: data
+            }
+        ]
+    };
+    // Enable data zoom when user click bar.
+    const zoomSize = 6;
+    chart1.on('click', function (params) {
+        console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
+        chart1.dispatchAction({
+            type: 'dataZoom',
+            startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+            endValue:
+                dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+        });
+    });
+
+    option && chart1.setOption(option);
+
+}
+
+
+
+
+
+
